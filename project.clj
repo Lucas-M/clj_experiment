36c55045656588929dec5647792849bb67379481
Luke <lucas.macd@gmail.com>
2014-08-18 21:54:54 -0400

    New Push

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.6"]
                  [hiccup "1.0.5"]
-                 [ring-server "0.3.1"]]
+                 [ring-server "0.3.1"]
+                 [com.datomic/datomic-free "0.9.4880.2"]
+                 [expectations "2.0.9"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler compojure_experiment.handler/app
          :init compojure_experiment.handler/init
--- test/compojure_experiment/test/handler.clj
+++ test/compojure_experiment/test/handler.clj
@@ -1,14 +1,34 @@
-(ns compojure_experiment.test.handler
-  (:use clojure.test
-        ring.mock.request
-        compojure_experiment.handler))
+;; (ns compojure_experiment.test.handler
+;;   (:use clojure.test
+;;         ring.mock.request
+;;         compojure_experiment.handler))
 
-(deftest test-app
-  (testing "main route"
-    (let [response (app (request :get "/"))]
-      (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
+;; (deftest test-app
+;;   (testing "main route"
+;;     (let [response (app (request :get "/"))]
+;;       (is (= (:status response) 200))
+;;       (is (= (:body response) "Hello World"))))
 
-  (testing "not-found route"
-    (let [response (app (request :get "/invalid"))]
-      (is (= (:status response) 404)))))
+;;   (testing "not-found route"
+;;     (let [response (app (request :get "/invalid"))]
+;;       (is (= (:status response) 404)))))
+
+(ns compojure_experiment.core-test
+  (:require [expectations :refer :all]
+            [compojure_experiment.core]
+            [datomic.api :as d]))
+
+(defn create-empty-in-memory-db []
+  (let [uri "datomic:mem://compojure-test-db"]
+    (d/delete-database uri)
+    (d/creat-database uir)
+    (let [conn (d/connect uri)
+          schema (load-file "resources/schema.edn")]
+      (d/transact conn schema)
+      conn)))
+
+(expect "John"
+        (with-redefs [conn (create-emtpy-in-memory-db)]
+          (do
+            (add-merchant "John")
+            (find--all-merchants)))
